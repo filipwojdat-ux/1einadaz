cad6f429652df8ad40ef96f01bdb5c222a8c5dad
Habibi <filip.wojdat@onet.eu>
2026-04-27 12:24:02 +0200

    Add files via upload

---
+++ Bryla.cs
@@ -0,0 +1,23 @@
+﻿namespace ConsoleApp5
+{
+
+    abstract class Bryla
+    {
+        protected double polePodstawy;
+
+    
+    public Bryla(double polePodstawy)
+        {
+            this.polePodstawy = polePodstawy;
+        }
+        public abstract double ObliczObjetosc();
+        public abstract double ObliczPoleCalosci();
+        public  double ObliczPolePodstawy()
+        {
+            return polePodstawy;
+        }
+
+        
+
+
+    } }
